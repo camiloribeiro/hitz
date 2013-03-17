59295f0e86eb341d40b1b4885018e079cb7ceb9a
camiloribeiro <cribeiro@cribeiro.(none)>
2013-03-17 01:47:25 -0300

    It's working, but it's still missing an way to run the counter with server side by a http respose

--- hitz/hitz.js
+++ hitz/hitz.js
@@ -1,19 +1,40 @@
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
+Loadserver = new Meteor.Collection("loadserver");
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to hitz.";
+    var servers = Loadserver.find({}).distinct('name', true).length;
+    var teste = '';
+    for (var i = 0; i < servers;i++ ){
+      var servidor = Loadserver.find({}).distinct('name', true)[i].name;
+      teste += servidor +" => "+ Loadserver.find({name: servidor}).count();
+      if (i + 1 != servers)
+        teste += " || ";
+    }
+    return teste;
   };
 
   Template.hello.events({
     'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      Loadserver.insert({name: getUrlVars()['param']});
     }
   });
+
+  Template.hello.rendered = function() {
+    if(!this._rendered) {
+      this._rendered = true;
+         Loadserver.insert({name: getUrlVars()['param']});
+    }
+  }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
